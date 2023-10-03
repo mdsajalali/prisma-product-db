43409825adb78e618dff9b21e9039e7fb5b7620c
mdsajalali <mdsajalali2020@gmail.com>
2023-10-03 14:34:43 +0600

    initial commit

---
+++ app/page.js
@@ -0,0 +1,10 @@
+const page = () => {
+  return (
+    <div className="flex items-center justify-center flex-col h-screen">
+      <h1 className="text-center text-5xl ">Assignment - 17</h1>
+      <h2 className="text-center text-3xl mt-5 ">Prisma Product Database!!</h2>
+    </div>
+  );
+};
+
+export default page;
